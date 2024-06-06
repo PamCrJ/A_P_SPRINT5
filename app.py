baa6dd85cebd5e8dd4013ffd4b2618c1ffb02cc3
PamCrJ <la.pamecrespo@gmail.com>
2024-06-06 13:53:58 -0600

    se añadió scatterplot

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
         
-car_data = pd.read_csv('vehicles_us.csv') # leer los datos
+car_data = pd.read_csv('../vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
         
 if hist_button: # al hacer clic en el botón
@@ -14,3 +14,13 @@ if hist_button: # al hacer clic en el botón
         
             # mostrar un gráfico Plotly interactivo
             st.plotly_chart(fig, use_container_width=True)
+
+scatter_button = st.button('Construir un scatterplot')
+
+if scatter_button:
+        
+        st.write('Creación de un gráfico de dispersión para visualizar el modelo - año de los autos en relación a los días que llevan anunciados')
+
+        fig = px.scatter(car_data, x="model_year", y="days_listed")
+
+        st.plotly_chart(fig, use_container_width=True)
